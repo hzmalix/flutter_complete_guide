a85076a94a1c842bf4f6e64eb6e4ec9e1af831b8
HAMMAD <hzmalix@gmail.com>
2022-01-25 19:28:31 +0400

    Named Function

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,46 @@
 import 'package:flutter/material.dart';
 
 // void main() {
-//   runApp(const MyApp()); 
+//   runApp(const MyApp());
 // }
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-
   const MyApp({Key? key}) : super(key: key);
+
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
 
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite animal?",
+    ];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('My First App'),),
-        body: const Text('This is my default text!'),
+        appBar: AppBar(
+          title: const Text('My First App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            const Text('The question!'),
+            ElevatedButton(
+              child: const Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: const Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: const Text('Answer 3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }
